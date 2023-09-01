815def4d4564ea2afef41e2f439c4080a60eb147
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-08-31 22:07:59 -0600

    More examples when a wrong date is sent

--- telegram/messagesHandler.ts
+++ telegram/messagesHandler.ts
@@ -4,15 +4,19 @@ import { users } from "../constants.ts";
 import nlToDate from "../utils/nlToDate.ts";
 import sendMovies from "../crons/sendMovies.ts";
 
+const ejemplos = ["En dos días", "El siguiente sábado", "Hoy"];
+
 bot.on("message", (ctx) => {
   (async () => {
     if (!users.includes(ctx.chat.id)) return;
     const { text } = ctx.message;
 
     const incorrectText = () =>
-      ctx.reply('¿De qué día quieres los resultados?\n\nPor ejemplo: "<code>En dos días</code>"', {
-        parse_mode: "HTML",
-      });
+      ctx.reply(
+        `¿De qué día quieres los resultados?\n\n` +
+          `Por ejemplo:\n - <code>${ejemplos.join("</code>\n - <code>")}</code>`,
+        { parse_mode: "HTML" }
+      );
 
     if (!text) return incorrectText();
 
