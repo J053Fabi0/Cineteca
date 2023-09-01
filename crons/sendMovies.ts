be6dd924ca6f498852b5a66407283340bcb685d3
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-08-31 22:09:22 -0600

    TryCatch when sending the movies to each user

--- crons/sendMovies.ts
+++ crons/sendMovies.ts
@@ -2,6 +2,7 @@ import { Moment } from "moment";
 import { users } from "../constants.ts";
 import bot from "../telegram/initBot.ts";
 import getMovies from "../utils/getMovies.ts";
+import handleError from "../utils/handleError.ts";
 import { sendHTMLMessage } from "../utils/sendMessage.ts";
 
 const picturesSent: Record<string, string> = {};
@@ -13,28 +14,32 @@ export default async function sendMovies(date: Moment, sendTo = users) {
   const dayName = date.format("dddd");
 
   for (const user of sendTo) {
-    await sendHTMLMessage(`Resultados del día <code>${dayName} ${day} de ${monthName}</code>`, user);
+    try {
+      await sendHTMLMessage(`Resultados del día <code>${dayName} ${day} de ${monthName}</code>`, user);
 
-    const movies = await getMovies(date);
-    const { length } = movies;
+      const movies = await getMovies(date);
+      const { length } = movies;
 
-    for (let i = 0; i < length; i++) {
-      const movie = movies[i];
-      // send the photo
-      if (movie.image) {
-        let caption = `${i + 1}/${length} <b>${movie.title}</b>\n\n`;
-        if (movie.description) caption += `${movie.description}\n\n`;
-        if (movie.schedule) caption += `<b>Horario:</b> ${movie.schedule}\n`;
-        if (movie.location) caption += `<b>Lugar:</b> ${movie.location}\n`;
-        if (movie.url) caption += `\n<a href="${movie.url}">Más información</a>`;
+      for (let i = 0; i < length; i++) {
+        const movie = movies[i];
+        // send the photo
+        if (movie.image) {
+          let caption = `${i + 1}/${length} <b>${movie.title}</b>\n\n`;
+          if (movie.description) caption += `${movie.description}\n\n`;
+          if (movie.schedule) caption += `<b>Horario:</b> ${movie.schedule}\n`;
+          if (movie.location) caption += `<b>Lugar:</b> ${movie.location}\n`;
+          if (movie.url) caption += `\n<a href="${movie.url}">Más información</a>`;
 
-        const { photo } = await bot.api.sendPhoto(user, picturesSent[movie.image] || movie.image, {
-          caption,
-          parse_mode: "HTML",
-        });
+          const { photo } = await bot.api.sendPhoto(user, picturesSent[movie.image] || movie.image, {
+            caption,
+            parse_mode: "HTML",
+          });
 
-        if (!picturesSent[movie.image]) picturesSent[movie.image] = photo[photo.length - 1].file_id;
+          if (!picturesSent[movie.image]) picturesSent[movie.image] = photo[photo.length - 1].file_id;
+        }
       }
+    } catch (e) {
+      handleError(e);
     }
   }
 }
