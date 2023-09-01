efac18874109d2f1caf0ce961ff113df91bdccf3
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-08-31 22:03:22 -0600

    sendMovies sends them to every user in sendTo

--- crons/sendMovies.ts
+++ crons/sendMovies.ts
@@ -12,27 +12,29 @@ export default async function sendMovies(date: Moment, sendTo = users) {
   const monthName = date.format("MMMM");
   const dayName = date.format("dddd");
 
-  await sendHTMLMessage(`Resultados del día <code>${dayName} ${day} de ${monthName}</code>`, sendTo[0]);
+  for (const user of sendTo) {
+    await sendHTMLMessage(`Resultados del día <code>${dayName} ${day} de ${monthName}</code>`, user);
 
-  const movies = await getMovies(date);
-  const { length } = movies;
+    const movies = await getMovies(date);
+    const { length } = movies;
 
-  for (let i = 0; i < length; i++) {
-    const movie = movies[i];
-    // send the photo
-    if (movie.image) {
-      let caption = `${i + 1}/${length} <b>${movie.title}</b>\n\n`;
-      if (movie.description) caption += `${movie.description}\n\n`;
-      if (movie.schedule) caption += `<b>Horario:</b> ${movie.schedule}\n`;
-      if (movie.location) caption += `<b>Lugar:</b> ${movie.location}\n`;
-      if (movie.url) caption += `\n<a href="${movie.url}">Más información</a>`;
+    for (let i = 0; i < length; i++) {
+      const movie = movies[i];
+      // send the photo
+      if (movie.image) {
+        let caption = `${i + 1}/${length} <b>${movie.title}</b>\n\n`;
+        if (movie.description) caption += `${movie.description}\n\n`;
+        if (movie.schedule) caption += `<b>Horario:</b> ${movie.schedule}\n`;
+        if (movie.location) caption += `<b>Lugar:</b> ${movie.location}\n`;
+        if (movie.url) caption += `\n<a href="${movie.url}">Más información</a>`;
 
-      const { photo } = await bot.api.sendPhoto(users[0], picturesSent[movie.image] || movie.image, {
-        caption,
-        parse_mode: "HTML",
-      });
+        const { photo } = await bot.api.sendPhoto(user, picturesSent[movie.image] || movie.image, {
+          caption,
+          parse_mode: "HTML",
+        });
 
-      if (!picturesSent[movie.image]) picturesSent[movie.image] = photo[photo.length - 1].file_id;
+        if (!picturesSent[movie.image]) picturesSent[movie.image] = photo[photo.length - 1].file_id;
+      }
     }
   }
 }
